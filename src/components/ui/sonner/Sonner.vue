ada4c10e00f5a43188de89d54c762fce4d54fbbb
Omona Coker <teminirecokef@gmail.com>
2025-10-02 14:32:24 +0100

    Sonner again

--- src/components/ui/sonner/Sonner.vue
+++ src/components/ui/sonner/Sonner.vue
@@ -1,13 +1,10 @@
-<script lang="ts" setup>
+<script setup lang="ts">
 import { Toaster as Sonner } from "vue-sonner"
-
-const props = defineProps<Record<string, unknown>>()
 </script>
 
 <template>
   <Sonner
     class="toaster group"
-    v-bind="props"
     :style="{
       '--normal-bg': 'var(--popover)',
       '--normal-text': 'var(--popover-foreground)',
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,9 @@
 
 declare module "vue-sonner";
 declare module "@iconify/vue";
-
+declare module "vue-sonner" {
+  export interface SonnerProps {
+    [key: string]: unknown
+  }
+}
 
